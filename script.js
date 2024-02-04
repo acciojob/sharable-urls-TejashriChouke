97976e47c871b9f8666348715b1d9c64e0f3c512
TejashriChouke <139746868+TejashriChouke@users.noreply.github.com>
2024-02-04 19:43:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // your code here
+document.getElementById('button').addEventListener('click', function() {
+    const name = document.getElementById('name').value;
+    const year = document.getElementById('year').value;
+    
+    // Craft the query string
+    const queryString = `name=${name} & year=${year}`;
+    
+    // Update the URL in the h3 element
+    document.getElementById('url').textContent = `https://localhost:8080/   ${queryString}`;
+});
